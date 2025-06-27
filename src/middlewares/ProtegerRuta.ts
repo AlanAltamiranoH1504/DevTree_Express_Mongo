729fa6e9b65d5ef94a47770bd63ffecf5c9d8fdb
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-06-27 10:26:51 -0600

    feat: add user search by handle - add endpoint for user search by parameter handle - add method in UserController to search for information according to handle provided

--- src/middlewares/ProtegerRuta.ts
+++ src/middlewares/ProtegerRuta.ts
@@ -43,7 +43,7 @@ const protegerRuta = async (req, res, next) => {
 
         const usuarioEnSesion = await Usuario.findOne({
             email: email,
-        }).select("nombre handle email descripcion imagen urlImagen");
+        }).select("nombre handle email descripcion imagen urlImagen links");
         if (!usuarioEnSesion) {
             return res.status(404).json({
                 error: "Usuario no encontrado"
--- src/routes/UsuarioRoutes.ts
+++ src/routes/UsuarioRoutes.ts
@@ -1,5 +1,10 @@
 import {Router} from "express";
-import {pruebaUsuarioController, updateInformacion, updateLinksUser} from "../controllers/UsuarioController";
+import {
+    informacionDePerfilPublica,
+    pruebaUsuarioController,
+    updateInformacion,
+    updateLinksUser
+} from "../controllers/UsuarioController";
 import {requestUpdateInformacionUsuario} from "../requests";
 import protegerRuta from "../middlewares/ProtegerRuta";
 const router = Router();
@@ -7,5 +12,6 @@ const router = Router();
 router.get("", pruebaUsuarioController);
 router.put("/", protegerRuta, requestUpdateInformacionUsuario, updateInformacion);
 router.put("/links", protegerRuta, updateLinksUser);
+router.get("/:handle", informacionDePerfilPublica);
 
 export default router;
